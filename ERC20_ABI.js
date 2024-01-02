49822011d6a6f55c28c0bad1dfafba33854f9916
Unreal Kingdoms <markchester@cayc.io>
2024-01-01 23:52:40 -0500

    abi

---
+++ ERC20_ABI.js
@@ -0,0 +1,21 @@
+var ERC20_ABI = [
+	 {
+		"constant": false,
+		"inputs": [
+		  {
+			"name": "_mintAmountSheesh",
+			"type": "uint256"
+		  },
+		  {
+			"name": "receiver",
+			"type": "address"
+		  }
+		],
+		"name": "publicMintWithSheesh",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	  }
+	 
+]
